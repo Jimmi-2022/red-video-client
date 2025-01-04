a8593f948c00420fce24ba6920ef91df0cfb2ba4
ser-evtushe <ser-evtushe@mail.ru>
2025-01-04 14:23:25 +0300

    13

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
 import type { Config } from 'tailwindcss'
+import plugin from 'tailwindcss/plugin'
 
 import { COLORS } from './src/constants/colors.constants'
 
-export default {
+const config: Config = {
 	content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
 		extend: {
@@ -18,5 +19,18 @@ export default {
 			}
 		}
 	},
-	plugins: []
-} satisfies Config
+	plugins: [
+		plugin(function ({ addUtilities }) {
+			addUtilities({
+				'.line-clamp-2': {
+					display: '-webkit-box',
+					'webkit-line-clamp': 2,
+					'webkit-box-orient': 'vertical',
+					overflow: 'hidden',
+					'text-overflow': 'ellipsis'
+				}
+			})
+		})
+	]
+}
+export default config
